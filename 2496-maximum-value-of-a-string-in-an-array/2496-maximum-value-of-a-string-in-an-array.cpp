78ce436d3758f04d43c47402cb1b7e416f000a6f
Mustafa Barış EMEKTAR <64257031+mb-emektar@users.noreply.github.com>
2023-03-08 19:53:04 +0300

    Time: 6 ms (26.42%), Space: 7.7 MB (78.12%) - LeetHub

--- 2496-maximum-value-of-a-string-in-an-array/2496-maximum-value-of-a-string-in-an-array.cpp
+++ 2496-maximum-value-of-a-string-in-an-array/2496-maximum-value-of-a-string-in-an-array.cpp
@@ -2,14 +2,14 @@ class Solution {
 public:
     int maximumValue(vector<string>& strs) {
         int out = 0;
+        int ctrl = 0;
         for(int i = 0 ; i < strs.size() ; i++ )
         {
-            int ctrl = 0;
-             for(int j = 0 ; j < strs[i].size() ; j++ )
+            ctrl = 0;
+            for(int j = 0 ; j < strs[i].size() ; j++ )
             {
                 if( (strs[i][j] < '0') || ('9' < strs[i][j]) )
                 {
-                    cout << strs[i].size();
                     if(out < strs[i].size() )
                     { 
                         out = strs[i].size();
@@ -22,9 +22,7 @@ public:
             if (ctrl == 0)
             {
                 if ( out < stoi(strs[i]) ) out = stoi(strs[i]);
-
             }
-
         }
 
         return out;
