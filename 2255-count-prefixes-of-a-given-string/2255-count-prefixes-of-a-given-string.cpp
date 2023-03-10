c176a7c986b209db83412969bd99ddd334deb762
Mustafa Barış EMEKTAR <64257031+mb-emektar@users.noreply.github.com>
2023-03-10 20:11:40 +0300

    Time: 12 ms (42.29%), Space: 12.1 MB (23.13%) - LeetHub

---
+++ 2255-count-prefixes-of-a-given-string/2255-count-prefixes-of-a-given-string.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int countPrefixes(vector<string>& words, string s) {
+        int out = words.size();
+        for(int i = 0 ; i < words.size() ; i++)
+        {
+            for(int j = 0 ; j < words[i].size() ; j++)
+            {
+                if( words[i][j] != s[j] )
+                {
+                    out--;        
+                    break;
+                }
+                
+
+            }
+        }
+        return out;
+    }
+};
